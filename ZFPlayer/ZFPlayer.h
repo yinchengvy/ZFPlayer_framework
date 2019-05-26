7a756eb7c3cc134dcc4382ffdeb13cb771770867
Yincheng <yinxion8@126.com>
2019-05-26 22:39:16 +0800

    Initial Commit

---
+++ ZFPlayer/ZFPlayer.h
@@ -0,0 +1,19 @@
+//
+//  ZFPlayer.h
+//  ZFPlayer
+//
+//  Created by Yincheng on 2019/5/26.
+//  Copyright © 2019 Yincheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZFPlayer.
+FOUNDATION_EXPORT double ZFPlayerVersionNumber;
+
+//! Project version string for ZFPlayer.
+FOUNDATION_EXPORT const unsigned char ZFPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZFPlayer/PublicHeader.h>
+
+
